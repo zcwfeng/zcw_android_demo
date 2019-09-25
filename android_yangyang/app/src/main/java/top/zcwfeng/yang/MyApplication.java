3f4efe9ad35841fa632f0a46fa5d1b88c9f5ed9a
zcwfeng <zcwfeng@126.com>
2019-09-26 00:33:42 +0800

    debug tree Timber add

--- android_yangyang/app/src/main/java/top/zcwfeng/yang/MyApplication.java
+++ android_yangyang/app/src/main/java/top/zcwfeng/yang/MyApplication.java
@@ -2,10 +2,28 @@ package top.zcwfeng.yang;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        } else {
+            Timber.plant(new CrashReportingTree());
+        }
+
+
+    }
+
+
+    private static class CrashReportingTree extends Timber.Tree {
+        @Override
+        protected void log(int priority, String tag, String message, Throwable t) {
+
+        }
     }
 }

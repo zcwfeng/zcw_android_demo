52da68bd3db8e693623c5d8f9ab484de061b1df0
zcwfeng <zcwfeng@126.com>
2020-06-19 17:14:22 +0800

    RxJava 重新整理demo，add图片

--- android_rxjava/app/src/main/java/top/zcwfeng/rxjava/MainActivity.java
+++ android_rxjava/app/src/main/java/top/zcwfeng/rxjava/MainActivity.java
@@ -1,116 +1,169 @@
 package top.zcwfeng.rxjava;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.os.Looper;
 import android.os.SystemClock;
+import android.text.TextPaint;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.concurrent.Callable;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
+import io.reactivex.ObservableTransformer;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableObserver;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "RxAndroidSamples";
-    private final CompositeDisposable disposables = new CompositeDisposable();
+    final static String PATH = "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1794202421,3172844000&fm=26&gp=0.jpg";
+    private ImageView image;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        testRxAndroidWorkRule();
-
-
+        image = findViewById(R.id.action_image);
 
     }
 
 
-    public void button_run_scheduler(View view){
-        onRunSchedulerExampleButtonClicked();
+    public void button_run_scheduler(View view) {
+        rxJavaDownloadImageAction(view);
     }
 
-    private void onRunSchedulerExampleButtonClicked() {
-
-        disposables.add(sampleObservable().subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribeWith(new DisposableObserver<String>() {
+    // 抽取订阅,切换线程 封装上游数据
+    //.subscribeOn(Schedulers.io())
+    //.observeOn(AndroidSchedulers.mainThread());
+    public static final <UD> ObservableTransformer<UD,UD> upstreamData(){
+        return new ObservableTransformer<UD, UD>() {
             @Override
-            public void onNext(String s) {
-                Log.d(TAG, "onNext(" + s + ")--->" + Thread.currentThread().getName());
-
-
+            public ObservableSource<UD> apply(Observable<UD> upstream) {
+                return upstream.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread()).map(new Function<UD, UD>() {
+                            @Override
+                            public UD apply(UD ud) throws Exception {
+                                Log.d(TAG, "apply: 监听切换线程。。。");
+                                return ud;
+                            }
+                        });
             }
-
-            @Override
-            public void onError(Throwable e) {
-                Log.e(TAG, "onError()--->" + Thread.currentThread().getName(), e);
-
-            }
-
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "onComplete()--->" + Thread.currentThread().getName());
-
-            }
-        }));
+        };
     }
 
-    public Observable<String> sampleObservable(){
-        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
-            @Override
-            public ObservableSource<? extends String> call() throws Exception {
-                SystemClock.sleep(5000);
-
-                return Observable.just("one", "two", "three", "four", "five");
-            }
-        });
+    private final Bitmap drawWaterMark(Bitmap bitmap,String text,Paint paint,int paddingLeft,int paddingTop) {
+        Bitmap.Config bitmapCofing = bitmap.getConfig();
+        paint.setDither(true);//获取清晰的采样图片
+        paint.setFilterBitmap(true);// 过滤
+        if(bitmapCofing == null){
+            bitmapCofing = Bitmap.Config.ARGB_8888;
+        }
+        bitmap = bitmap.copy(bitmapCofing,true);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawText(text,paddingLeft,paddingTop,paint);
+        return bitmap;
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        disposables.clear();
+    private void rxJavaDownloadImageAction(View view) {
+
+//        请求----->卡片（map）执行请求
+//        --------> 终点 更新UI
+
+        // 起点
+        // 第二步
+        Observable.just(PATH)
+                // 根据上层变化变化，卡片map
+                // 第三步
+                .map(new Function<String, Bitmap>() {
+                    @Override
+                    public Bitmap apply(String s) throws Exception {
+                        URL url = new URL(PATH);
+                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                        httpURLConnection.setReadTimeout(5000);
+                        httpURLConnection.connect();
+                        int responseCode = httpURLConnection.getResponseCode();
+                        if (responseCode == HttpURLConnection.HTTP_OK) {
+                            InputStream inputStream = httpURLConnection.getInputStream();
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            return bitmap;
+                        }
+                        return null;
+                    }
+                })
+
+
+
+                .map(new Function<Bitmap, Bitmap>() {
+                    @Override
+                    public Bitmap apply(Bitmap bitmap) throws Exception {
+                        Paint paint = new TextPaint();
+                        paint.setTextSize(88);
+                        paint.setColor(Color.RED);
+                        Bitmap filterBitmap = drawWaterMark(bitmap,"RxJava",paint,88,88);
+                        return filterBitmap;
+                    }
+                })
+                .map(new Function<Bitmap, Bitmap>() {
+                    @Override
+                    public Bitmap apply(Bitmap bitmap) throws Exception {
+                        Log.d(TAG, "apply: 下载图片：" + SystemClock.elapsedRealtimeNanos());
+                        return bitmap;
+                    }
+                })
+                .compose(MainActivity.<Bitmap>upstreamData())
+
+                //终点 起点 订阅
+                .subscribe(
+
+                        /* 终点 */
+                        new Observer<Bitmap>() {
+                            /* 订阅开始 */
+                            @Override
+                            public void onSubscribe(Disposable d) {
+                                // 第一步
+                                progressDialog = new ProgressDialog(MainActivity.this);
+                                progressDialog.setTitle("下载");
+                                progressDialog.show();
+                            }
+
+                            /* 拿到事件 */
+                            // 第四步
+                            @Override
+                            public void onNext(Bitmap bitmap) {
+                                image.setImageBitmap(bitmap);
+                            }
+
+                            /* 错误 */
+                            @Override
+                            public void onError(Throwable e) {
+
+                            }
+
+                            //完成 第五步
+                            @Override
+                            public void onComplete() {
+                                if (progressDialog != null)
+                                    progressDialog.dismiss();
+                            }
+                        });
+
+
     }
 
-    private void testRxAndroidWorkRule(){
-        Looper backgroundLooper = getMainLooper();
-        Observable.just("one", "two", "three", "four", "five")
-                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.from(backgroundLooper))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        Log.e("rxjava_zcw","onSubscri:"+d.toString());
-                    }
 
-                    @Override
-                    public void onNext(String s) {
-                        Log.e("rxjava_zcw","onNext:" + s);
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e("rxjava_zcw","onError:"+e.getMessage());
-
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        Log.e("rxjava_zcw","onComplete:");
-
-                    }
-                });
-
-    }
 }
---
+++ android_rxjava/app/src/main/java/top/zcwfeng/rxjava/OldMainActivity.java
@@ -0,0 +1,116 @@
+package top.zcwfeng.rxjava;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class OldMainActivity extends AppCompatActivity {
+    private static final String TAG = "RxAndroidSamples";
+    private final CompositeDisposable disposables = new CompositeDisposable();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        testRxAndroidWorkRule();
+
+
+
+    }
+
+
+    public void button_run_scheduler(View view){
+        onRunSchedulerExampleButtonClicked();
+    }
+
+    private void onRunSchedulerExampleButtonClicked() {
+
+        disposables.add(sampleObservable().subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeWith(new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "onNext(" + s + ")--->" + Thread.currentThread().getName());
+
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError()--->" + Thread.currentThread().getName(), e);
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete()--->" + Thread.currentThread().getName());
+
+            }
+        }));
+    }
+
+    public Observable<String> sampleObservable(){
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override
+            public ObservableSource<? extends String> call() throws Exception {
+                SystemClock.sleep(5000);
+
+                return Observable.just("one", "two", "three", "four", "five");
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+    }
+
+    private void testRxAndroidWorkRule(){
+        Looper backgroundLooper = getMainLooper();
+        Observable.just("one", "two", "three", "four", "five")
+                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.from(backgroundLooper))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.e("rxjava_zcw","onSubscri:"+d.toString());
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.e("rxjava_zcw","onNext:" + s);
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("rxjava_zcw","onError:"+e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e("rxjava_zcw","onComplete:");
+
+                    }
+                });
+
+    }
+}

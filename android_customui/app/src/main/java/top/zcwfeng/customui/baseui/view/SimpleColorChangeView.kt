8350d6b5da845e6074bd419999269edb9770f898
zcwfeng <zcwfeng@126.com>
2020-08-18 18:17:14 +0800

    整理demo，整理gradle文件格式。添加辅助demo 抢红包2017

--- android_customui/app/src/main/java/top/zcwfeng/customui/baseui/view/SimpleColorChangeView.kt
+++ android_customui/app/src/main/java/top/zcwfeng/customui/baseui/view/SimpleColorChangeView.kt
@@ -19,9 +19,9 @@ class SimpleColorChangeView : AppCompatTextView {
         }
 
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
---
+++ android_customui/robmoneydemo/src/main/java/top/zcwfeng/robmoneydemo/RobMoney.java
@@ -0,0 +1,118 @@
+package top.zcwfeng.robmoneydemo;
+
+import android.accessibilityservice.AccessibilityService;
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.List;
+
+public class RobMoney extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        int eventType = event.getEventType();
+        switch (eventType) {
+            //第一步：监听通知栏消息
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                List<CharSequence> texts = event.getText();
+                if (!texts.isEmpty()) {
+                    for (CharSequence text : texts) {
+                        String content = text.toString();
+                        Log.i("demo", "text:" + content);
+                        if (content.contains("[微信红包]")) {
+                            //模拟打开通知栏消息
+                            if (event.getParcelableData() != null
+                                    &&
+                                    event.getParcelableData() instanceof Notification) {
+                                Notification notification = (Notification) event.getParcelableData();
+                                PendingIntent pendingIntent = notification.contentIntent;
+                                try {
+                                    pendingIntent.send();
+                                } catch (PendingIntent.CanceledException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            //第二步：监听是否进入微信红包消息界面
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                String className = event.getClassName().toString();
+                if (className.equals("com.tencent.mm.ui.LauncherUI")) {
+                    //开始抢红包
+                    getPacket();
+                } else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")) {
+                    //开始打开红包
+                    openPacket();
+                }
+                break;
+        }
+    }
+
+    /**
+     * 查找到
+     */
+    @SuppressLint("NewApi")
+    private void openPacket() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo != null) {
+            List<AccessibilityNodeInfo> list = nodeInfo
+                    .findAccessibilityNodeInfosByText("抢红包");
+            for (AccessibilityNodeInfo n : list) {
+                n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+
+    }
+
+    @SuppressLint("NewApi")
+    private void getPacket() {
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        recycle(rootNode);
+    }
+
+    /**
+     * 打印一个节点的结构
+     *
+     * @param info
+     */
+    @SuppressLint("NewApi")
+    public void recycle(AccessibilityNodeInfo info) {
+        if (info.getChildCount() == 0) {
+            if (info.getText() != null) {
+                if ("领取红包".equals(info.getText().toString())) {
+                    //这里有一个问题需要注意，就是需要找到一个可以点击的View
+                    Log.i("demo", "Click" + ",isClick:" + info.isClickable());
+                    info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    AccessibilityNodeInfo parent = info.getParent();
+                    while (parent != null) {
+                        Log.i("demo", "parent isClick:" + parent.isClickable());
+                        if (parent.isClickable()) {
+                            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            break;
+                        }
+                        parent = parent.getParent();
+                    }
+
+                }
+            }
+
+        } else {
+            for (int i = 0; i < info.getChildCount(); i++) {
+                if (info.getChild(i) != null) {
+                    recycle(info.getChild(i));
+                }
+            }
+        }
+    }
+
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
